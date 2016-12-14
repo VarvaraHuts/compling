046bb87d12e38e091fc1d07740e1e914afb281b7
Varvara <varvarahuts@gmail.com>
2016-12-14 23:16:22 +0300

    Delete regexp6.py

--- regexp6.py
+++
@@ -1,9 +0,0 @@
-import re
-f = open ('1.txt', 'r', encoding = 'utf-8')
-s = ' '.join(f)
-words = s.split()
-r = '(\"[A-Za-z]+\")'
-for word in words:
-    if re.search(r, word):
-        res = re.search (r, word)
-        print (res.group(1))
