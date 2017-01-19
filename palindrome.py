cf4ea377d05705d9c23be64dc3db5c46aeb4ed57
Varvara <varvarahuts@gmail.com>
2017-01-19 15:58:24 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -5,9 +5,9 @@ while True:
         break
     for i in range (len (s) - 1):
         if s[i] == s[len(s) - 1 - i]:
-            a = '+ palyndrome'
+            a = '+ palindrome'
         else:
-            a = '- palyndrome'
+            a = '- palindrome'
     f.write(s + ',   ' + str(len(s)) + ',   ' + a + '\n')
     s = input ()
 f.close()
