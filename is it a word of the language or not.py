2ab8d8e9236657528f04836a184c85d5330bd5ed
Varvara <varvarahuts@gmail.com>
2017-01-19 15:20:00 +0300

    Update is it a word of the language or not.py

--- is it a word of the language or not.py
+++ is it a word of the language or not.py
@@ -1,9 +1,14 @@
 s = input ()
-vowels = 'aeiouy'
-lastLetter = s[len(s)-1]
-for Letter in s:
-    if Letter in vowels and Letter != lastLetter:
-        print ('no')
-    else:
-        print ('yes')
+
+def lang(s):
+    vowels = 'aeiouy'
+    lastLetter = s[len(s)-1]
+    for Letter in s:
+        if Letter in vowels and Letter != lastLetter:
+            print ('no')
+        else:
+            print ('yes')
+    return
+
+lang(s)
 
