b98977f5a33f002235d9604979ec41ebaf6b1e12
Varvara <varvarahuts@gmail.com>
2017-01-19 16:08:56 +0300

    Update транслитерация грузиницы.py

--- транслитерация грузиницы.py
+++ транслитерация грузиницы.py
@@ -1,19 +1,23 @@
-def createDic():
+def openFile():
+    f = open('транслитерация грузиницы. текст.txt', 'r', encoding = 'utf-8')
+    f1 = f.read()
+    f.close()
+    return(f1)
+
+def transliterate(f1):
     dic = {'ა':'ɑ','ბ':'b','გ':'g','დ':'d','ე':'ɛ','ვ':'v','ზ':'z',
        'ი':'ɪ','ლ':'l','მ':'m','ნ':'n','ო':'ɔ','ჟ':'ʒ','რ':'r',
        'ს':'s','ჳ':'w','უ':'u','ღ':'ʁ','შ':'ʃ','ხ':'χ','ჴ':'q',
        'ჰ':'h','თ':'tʰ','კ':'kʼ','პ':'pʼ','ტ':'tʼ','ფ':'pʰ',
        'ქ':'kʰ','ყ':'qʼ','ჩ':'t͡ʃ','ც':'t͡s','ძ':'d͡z','წ':'t͡sʼ',
        'ჭ':'t͡ʃʼ','ჯ':'d͡ʒ'}
-    return (dic)
-
-def transliterate():
-    f = open('грузинский. текст.txt', 'r', encoding = 'utf-8')
-    f1 = f.read()
-    f.close()
-    for key, value in createDic().items():
+    for key, value in dic.items():
         f1 = f1.replace(key, value)
     return(f1)
 
+def main():
+    val = openFile()
+    print (transliterate(val))
+
 if __name__ == '__main__':
-    print (transliterate())
+    main()
