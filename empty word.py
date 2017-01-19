f15e61085021f1846154e66673efe6e69943fe0b
Varvara <varvarahuts@gmail.com>
2017-01-19 15:17:50 +0300

    Update empty word.py

--- empty word.py
+++ empty word.py
@@ -1,10 +1,21 @@
 s = input()
-lst = []
-while s:
-    if len(s) == 0:
-        break
-    lst.append(s)
-    s = input()
-lst.reverse()
-for word in lst:
-    print (word, end='\n')
+
+def create_array(s):
+    words = []
+    while s:
+        if len(s) == 0:
+            break
+        words.append(s)
+        s = input()
+    return(words)
+
+def reverse_words(words):
+    words.reverse()
+    return (words)
+
+def main():
+    val = create_array(s)
+    print(reverse_words(val))
+
+if __name__ == '__main__':
+    main()
