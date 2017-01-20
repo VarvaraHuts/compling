3f6c0f65387938970ad404367ce9d3eb2abe1e05
Varvara <varvarahuts@gmail.com>
2017-01-20 13:50:34 +0300

    Delete 0.py

--- 0.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import urllib.request
-
-resp = urllib.request.urlopen('http://www.lib.ru/LITRA/CHEHOW/kashtanka.txt')
-html = resp.read().decode('utf-8')
-resp.close()
-
-time.sleep(5)
