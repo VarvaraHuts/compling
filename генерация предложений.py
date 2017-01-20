ef002fc809e4e934704481d76f1cff1e400441b7
Varvara <varvarahuts@gmail.com>
2017-01-20 13:47:20 +0300

    Update генерация предложений.py

--- генерация предложений.py
+++ генерация предложений.py
@@ -1,44 +1,42 @@
 import random
 index = [0, 1, 2, 3]
 
-def openFile():
+def split_words():
     f = open ('дз1. словарь.txt', encoding = 'utf-8')
     f = f.read()
     words = f.split()
     return(words)
 
-def slotSubj():
+def slotSubj(words):
     subj = []
-    for word in openFile():
+    for word in words:
         if word.endswith('а') or word.endswith('я'):
             subj.append(word)
     i = random.choice(index)
     return (subj[i])
 
-def slotVerb():
+def slotVerb(words):
     verb = []
-    for word in openFile():
+    for word in words:
         if word.endswith('ит') or word.endswith('ет') or word.endswith('ёт'):
             verb.append(word)
     j = random.choice(index)
     return (verb[j])
 
-def slotObj():
+def slotObj(words):
     obj = []
-    for word in openFile():
+    for word in words:
         if word.endswith('у') or word.endswith('и'):
             obj.append(word)
     k = random.choice(index)
     return(obj[k])
 
-def createSent():
-    slot1 = slotSubj()
-    slot2 = slotVerb()
-    slot3 = slotObj()
-    return(slot1 + ' ' + slot2 + ' ' + slot3)
+def main():
+    val = split_words()
+    print (slotSubj(val)+ ' ' + slotVerb(val) + ' ' + slotObj(val))
 
-if __name__ == "__main__":
-    print (createSent())
+if __name__ == '__main__':
+    main()
     
     
 
