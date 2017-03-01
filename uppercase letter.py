1f44e4bb760215be4c9696bfa0f85dac532de550
Varvara <varvarahuts@gmail.com>
2017-03-01 15:36:18 +0300

    Delete uppercase letter.py

--- uppercase letter.py
+++
@@ -1,9 +0,0 @@
-f = open('1.txt')
-f = f.read()
-words = f.split()
-count_words = 0
-for word in words:
-    letter = word[0]
-    if letter.isupper():
-        count_words += 1
-print (count_words)
