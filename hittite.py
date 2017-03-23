10bf4d7ec0d7b246725fc5ff7dcd4ba6e1c5bb9e
Varvara <varvarahuts@gmail.com>
2017-03-23 18:21:39 +0300

    Update hittite.py

--- hittite.py
+++ hittite.py
@@ -1,33 +1,43 @@
-### это кусочек кода для определения языка 
+### это кусочек кода для определения языка и нахождения слов
 
-string = open("syllab.txt", "r", encoding="utf-8")
+string = open("syllab.html", "r", encoding="utf-8")
 string = string.read()
 
-automata = [(1, {"other":1, "U":2, "T":2,"<":3}),(2, {"eps":1}),
+automata = [(1, {"other":1, "U":2, "Ú":2, "T":2, "K":2, "R":2, "É":2, "I":2, "N":2, "<":3, " ":22}),(2, {"eps":1}),
             (3, {"i":4, "s":11, "other":21}), 
-            (4, {">":5, "other":21}),(5, {"other":5, "m":6, "a":6, "n":6, "k":6, "u":6, "w":6, "p":6, "w":6, "í":6, "š":6,
-                "i":6, "l":6, "ḫ":6, "z":6, "Š":7, "I":7,"<":8}),
-            (6, {"eps":5}), (7, {"eps":5}), (8, {'/':9}), (9, {'i':10}), (10, {'>':1}),
-            (11, {'u':12, 'other':21}), (12, {'p':13, 'other':21}), (13, {'>':14, 'other':21}),
-            (14, {'d':15, "<":16}), (15, {"eps":14}), (16, {'/':17, "other":21}), (17, {'s':18}), (18, {'u':19}), (19, {'p':20}), (20, {'>':1}),
-            (21, {">":1, "other":21})]
+            (4, {">":23, "other":21}),(5, {" ":23, "\n":23, "other":5, "m":6, "a":6, "n":6, "k":6, "u":6, "w":6, "p":6, "w":6, "í":6, "š":6,
+                "i":6, "l":6, "ḫ":6, "z":6, "d":6, "e":6, "t":6, "r":6, "á":6, "Š":7, "I":7, "A":7, "<":8}),
+            (6, {"eps":5}), (7, {"eps":5}), (8, {'/':9}), (9, {'i':10}), (10, {'>':22}),
+            (11, {'u':12, 'other':21}), (12, {'p':13, 'other':21}), (13, {'>':24, 'other':21}),
+            (14, {" ":24, 'd':15, "<":16}), (15, {"eps":14}), (16, {'/':17, "other":21}), (17, {'s':18}), (18, {'u':19}), (19, {'p':20}), (20, {'>':1}),
+            (21, {">":22, "other":21}), (22, {"eps":1}), (23, {"eps":5}), (24, {"eps":14})]
 
 hit = []
 shum = []
 akk = []
+word = [""]
 
 def handle_state(cur_state, cur_symbol):
         if cur_state == 1:
             pass
         if cur_state == 2:
             shum.append(cur_symbol)
+            word[len(word) - 1] += cur_symbol
         if cur_state == 6:
             hit.append(cur_symbol)
+            word[len(word) - 1] += cur_symbol
         if cur_state == 7:
             akk.append(cur_symbol)
+            word[len(word) - 1] += cur_symbol
         if cur_state == 15:
             hit.append(cur_symbol)
-
+            word[len(word) - 1] += cur_symbol
+        if cur_state == 22 and word[len( word) - 1] != "":
+            word.append("")
+        if cur_state == 23 and word[len( word) - 1] != "":
+            word.append("")
+        if cur_state == 24 and word[len( word) - 1] != "":
+            word.append("")
 
 cur_state = 1
 cur_symbol = 0
@@ -51,5 +61,6 @@ while cur_symbol < len(string):
 print (hit)
 print (shum)
 print (akk)
+print (word)
 
 
