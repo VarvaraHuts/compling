0332c15391d491c3c9ad1e790f4a0ea16ed9a3ba
Varvara <varvarahuts@gmail.com>
2017-04-08 00:00:23 +0300

    Update vk.api.py

--- hw4/vk.api.py
+++ hw4/vk.api.py
@@ -4,55 +4,58 @@ import urllib.request
 import json
 import sys
 
-s = open('posts.txt', 'w', encoding = 'utf-8')
-f = open('comments.txt', 'w', encoding = 'utf-8')
-
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 id_arr = []
 
-for i in range(201):
-    try:
-        url = 'https://api.vk.com/method/wall.get?owner_id=-37008294&count=1' + '&offset=' + str(i)
-        res = urllib.request.urlopen(url).read().decode('utf-8').translate(non_bmp_map)
-        data = json.loads(res)
-        text = data['response'][1]['text']
-        s.write(text + '\n')
 
-        post_id = data['response'][1]['id']
-        id_arr.append(post_id)
-    except: 
-        pass
-s.close()
-
-for x in id_arr:
-    for y in range(101):
+def write_vk_info_to_text_file(filename, url, count, is_post):
+    ids = []
+    s = open(filename, 'w', encoding='utf-8')
+    for i in range(count):
+        url_extended = url + str(i)
         try:
-            url_com = 'https://api.vk.com/method/wall.getComments?owner_id=-37008294&post_id=' + str(x) + '&count=1' + '&offset=' + str(y)
-            res_com = urllib.request.urlopen(url_com).read().decode('utf-8').translate(non_bmp_map)
-            data_com = json.loads(res_com)
-            for k in range(1,10):
-                text_com = data_com['response'][k]['text']
-                f.write(text_com + '\n')
+            res = urllib.request.urlopen(url_extended).read().decode('utf-8').translate(non_bmp_map)
+            data = json.loads(res)
+            if is_post:
+                text = data['response'][1]['text']
+                s.write(text + '\n')
+                post_id = data['response'][1]['id']
+                ids.append(post_id)
+            else:
+                for k in range(1, 10):
+                    text_com = data['response'][k]['text']
+                    s.write(text_com + '\n')
         except:
             pass
-f.close()
 
-a = open('posts.csv', 'w', encoding = 'Windows-1251')
-z = open('comments.csv', 'w', encoding = 'Windows-1251')
+    s.close()
+    return ids
 
-b = open('posts.txt', 'r', encoding = 'utf-8')
-lines = b.readlines()
-for line in lines:
-    line = line.replace('<br>', '')
-    words = line.split()
-    a.write(str(len(words)) + '\n')
-a.close()
 
-v = open('comments.txt', 'r', encoding = 'utf-8')
-lines = v.readlines()
-for line in lines:
-    words = line.split()
-    z.write(str(len(words)) + '\n')
-z.close()
-        
+def write_from_txt_to_csv(filename_txt, filename_csv, is_post):
+    a = open(filename_csv, 'w', encoding='Windows-1251')
+    b = open(filename_txt, 'r', encoding='utf-8')
+    lines = b.readlines()
+    for line in lines:
+        if is_post:
+            line = line.replace('<br>', '')
+        words = line.split()
+        a.write(str(len(words)) + '\n')
+    a.close()
+    b.close()
 
+
+if __name__ == "__main__":
+    ids = write_vk_info_to_text_file('posts.txt',
+                                'https://api.vk.com/method/wall.get?owner_id=-37008294&count=1&offset=',
+                                201,
+                                True)
+    for x in ids:
+        write_vk_info_to_text_file('comments.txt',
+                              'https://api.vk.com/method/wall.getComments?owner_id=-37008294&post_id=' +
+                              str(x) + '&count=1&offset=',
+                              101,
+                              False)
+
+    write_from_txt_to_csv('posts.txt', 'posts.csv', True)
+    write_from_txt_to_csv('comments.txt', 'comments.csv', True)
