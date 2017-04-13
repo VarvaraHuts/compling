f80e80e1ebc547b4d27fc0c19a7e9ea026e6d35b
Varvara <varvarahuts@gmail.com>
2017-04-13 08:31:21 +0300

    Update and rename транслитерация грузиницы.py to georgian transliteration.py

--- georgian transliteration.py
+++ georgian transliteration.py
@@ -1,5 +1,5 @@
 def openFile():
-    f = open('транслитерация грузиницы. текст.txt', 'r', encoding = 'utf-8')
+    f = open('georg text.txt', 'r', encoding = 'utf-8')
     f1 = f.read()
     f.close()
     return(f1)
