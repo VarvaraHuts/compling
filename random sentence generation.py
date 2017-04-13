c3c292503db7cc19473debca2cac32cd4666e310
Varvara <varvarahuts@gmail.com>
2017-04-13 08:36:05 +0300

    Update random sentence generation.py

--- random sentence generation.py
+++ random sentence generation.py
@@ -2,7 +2,7 @@ import random
 index = [0, 1, 2, 3]
 
 def split_words():
-    f = open ('rand sent dic.txt', encoding = 'utf-8')
+    f = open ('ran sent dic.txt', encoding = 'utf-8')
     f = f.read()
     words = f.split()
     return(words)
